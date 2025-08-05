8beed4067116185fd476f6aaef82aae339ec0e51
fmip313frontend@gmail.com <fmip313frontend@gmail.com>
2025-08-05 20:56:45 +0300

    Edit app.js

--- app.js
+++ app.js
@@ -1,37 +1,44 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-require('express-async-errors')
-const cors = require('cors') 
-const {TaskConnect} = require('./connections/TaskConection')
-const TaskRouter = require('./routes/TaskRoute')
-const notFound = require('./Middleware/notFound')
-const errorHandle = require('./Middleware/errorHandle')
-const AuthRouter = require('./routes/AuthRoute')
-
-const port = process.env.PORT || 5000
-
-
-
-//Middleware
-app.use(express.json())
-app.use(cors())
+const express = require("express");
+const app = express();
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const limitter = require("express-rate-limit");
+const sanitize = require("express-mongo-sanitize");
+require("dotenv").config();
+require("express-async-errors");
+//Builtin Middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(sanitize());
+app.use(
+  limitter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
 
 //Routes
-app.use('/api/v1/tasks', TaskRouter)
-app.use('/api/v1/auth', AuthRouter)
+const TaskRouter = require("./routes/TaskRoute");
+const AuthRouter = require("./routes/AuthRoute");
+app.use("/api/v1/tasks", TaskRouter);
+app.use("/api/v1/auth", AuthRouter);
 
-app.use(notFound)
-app.use(errorHandle)
-
+//Error Handle
+const notFound = require("./Middleware/notFound");
+const errorHandle = require("./Middleware/errorHandle");
+app.use(notFound);
+app.use(errorHandle);
 
 //Start
+const { TaskConnect } = require("./connections/TaskConection");
 const start = async (url) => {
-  await TaskConnect(url)
-  app.listen(port,()=>{
-      console.log(`Running On ${port}`)
-  })
-}
+  await TaskConnect(url);
+  app.listen(port,'0.0.0.0', () => {
+    console.log(`Running On ${port}`);
+  });
+};
 
-start(process.env.MONGO_URI)
-
+start(process.env.MONGO_URI);
